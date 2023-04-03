271aad339e5792f8d62f22e7f9d5e46f29997f9e
Ben Tenzlinger <btenzlin@1-D-29.42heilbronn.de>
2023-04-03 18:32:06 +0200

    basic profile page (html + css)

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -22,6 +22,11 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+
+      // {
+      //   test: /\.css?$/,
+      //   use: ['style-loader', 'css-loader'],
+      // },
     ],
   },
   resolve: {
