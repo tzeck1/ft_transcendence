7c673ff92108da0487d35757452ef015e0f2bdc1
Tom Zeck <tzeck@3-C-6.42heilbronn.de>
2023-04-25 19:38:13 +0200

    killed everything and started with tailwind [wip]

--- frontend/src/components/Game/StartGame.vue
+++ frontend/src/components/Game/StartGame.vue
@@ -1,112 +1,67 @@
 <template>
 	<div class="start_game">
-		<div class="container">
-			<transition name="slide">
-				<div v-if="showComp" class="comp-container" @click="selectCompetitive">
-					<img src="../../assets/pong.gif">
-					<span>Competetive</span>
-				</div>
-			</transition>
-			<transition name="slide">
-				<div v-if="showFun" class="fun-container">
-					<img src="../../assets/pong.gif">
-					<span>Fun Mode</span>
-				</div>
-			</transition>
-			<transition name="slide">
-				<button v-if="showStart" class="start-game-button">Start Game</button>
-			</transition>
+		<div class="slideshow">
+			<div class="comp-block block-style" @click="toggleFunBlock">
+				<img src="../../assets/pong.gif" class="block-image">
+				<span class="block-title">Competitive</span>
+			</div>
+			<div :class="['fun-block', 'block-style', funBlockClass]">
+				<img src="../../assets/pong.gif" class="block-image">
+				<span class="block-title">Fun Mode</span>
+			</div>
 		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, computed } from 'vue'
 
-const showComp = ref(true);
-const showFun = ref(true);
-const showStart = ref(false);
+const funBlockVisible = ref(true)
 
-const selectCompetitive = () => {
-	showFun.value = !showFun.value;
-	showStart.value = !showStart.value;
-};
+function toggleFunBlock() {
+	funBlockVisible.value = !funBlockVisible.value
+}
+
+const funBlockClass = computed(() => {
+	return funBlockVisible.value ? 'fun-block-visible' : 'fun-block-hidden'
+})
 </script>
 
 <style scoped>
-	.start_game {
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		-ms-transform: translate(-50%, -50%);
-		transform: translate(-50%, -50%);
-	}
-	
-	.container {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		margin-right: 2vw;
-		margin-left: 2vw;
-		overflow: hidden;
-		position: relative;
-	}
-	
-	.comp-container, .fun-container {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		font-size: 2.5vh;
-		padding: 15vh 2vh;
-		border-radius: 25px;
-		margin: 0 1vw;
-		/* position: absolute; */
-	}
-	
-	.comp-container img, .fun-container img {
-		position: relative;
-		transition: transform 0.3s ease-in-out;
-		transform-origin: center center;
+	/* .start_game {
+		@apply ;
+	} */
+
+	.slideshow {
+		@apply border mx-20 overflow-hidden flex items-center justify-center;
+
 	}
 
-	.comp-container:hover, .fun-container:hover {
-		background-color: rgba(255, 255, 255, 0.1);
+	.block-style {
+		@apply w-full md:w-1/3 lg:w-1/3 mx-2 px-2 py-60 bg-transparent transition-all duration-300 ease-in-out rounded-lg;
 	}
 
-	.comp-container:hover img, .fun-container:hover img {
-		transform: scale(1.2);
+	.block-style:hover {
+		@apply transform bg-white bg-opacity-10 scale-110;
 	}
 
-	.start-game-button {
-		position: absolute;
-		left: 50%;
-		transform: translateX(-50%);
-		padding: 1.5vh 2vh;
-		border-radius: 25px;
-		opacity: 0;
+	.block-image {
+		@apply w-full h-auto;
 	}
 
-	.slide-enter-active, .slide-leave-active {
-		transition: transform 1s, opacity 1s;
+	.block-title {
+		@apply block text-center mt-4;
 	}
 
-	.slide-enter {
-		transform: translateX(100%);
-		opacity: 0;
+	.fun-block-visible {
+		@apply opacity-100 translate-x-0;
 	}
 
-	.slide-enter-to {
-		transform: translateX(0);
-		opacity: 1;
+	.fun-block-hidden {
+		@apply opacity-100 transform translate-x-full;
 	}
 
-	.slide-leave {
-		transform: translateX(0);
-		opacity: 1;
-	}
-
-	.slide-leave-to {
-		transform: translateX(100%);
-		opacity: 0;
+	.block-style {
+		@apply transition-all duration-1000 ease-in-out;
 	}
 </style>
