10f8590e43d1a7ffac7356ab5dbb93ce0661aa11
Ben Tenzlinger <btenzlin@1-D-29.42heilbronn.de>
2023-04-26 15:06:28 +0200

    added game mode animations

--- frontend/src/components/Game/StartGame.vue
+++ frontend/src/components/Game/StartGame.vue
@@ -1,40 +1,83 @@
 <template>
-	<div class="start_game">
 		<div class="slideshow">
-			<div class="comp-block block-style" @click="toggleFunBlock">
+			<div :class="['block-style', compSetClass]">
+				<button class="block-image"> Start Game </button>
+				<span class="block-title">Settings</span>
+			</div>
+			<div :class="['comp-block', 'block-style', compBlockClass]" @click="selectCompBlock">
 				<img src="../../assets/pong.gif" class="block-image">
 				<span class="block-title">Competitive</span>
 			</div>
-			<div :class="['fun-block', 'block-style', funBlockClass]">
+			<div :class="['fun-block', 'block-style', funBlockClass]" @click="selectFunBlock">
 				<img src="../../assets/pong.gif" class="block-image">
 				<span class="block-title">Fun Mode</span>
 			</div>
+			<div :class="['fun-block-settings', 'block-style', funSetClass]">
+				<span class="block-title">Settings</span>
+			</div>
 		</div>
-	</div>
 </template>
+
 
 <script setup lang="ts">
 import { ref, computed } from 'vue'
 
-const funBlockVisible = ref(true)
+const funBlockVisible = ref(true);
+const compBlockVisible = ref(true);
+const compBlockSelected = ref(false);
+const funBlockSelected = ref(false);
 
-function toggleFunBlock() {
+function selectCompBlock() {
+	if (funBlockSelected.value)
+		return ;
 	funBlockVisible.value = !funBlockVisible.value
+	compBlockSelected.value = !compBlockSelected.value;
 }
 
+function selectFunBlock() {
+	if (compBlockSelected.value)
+		return ;
+	compBlockVisible.value = !compBlockVisible.value;
+	funBlockSelected.value = !funBlockSelected.value;
+}
+
+const compBlockClass = computed(() => {
+	if (funBlockSelected.value)
+		return ('comp-block-hidden');
+	else if (funBlockVisible.value)
+		return ('comp-block-visible');
+	return ('comp-block-selected');
+})
+
 const funBlockClass = computed(() => {
-	return funBlockVisible.value ? 'fun-block-visible' : 'fun-block-hidden'
+	if (compBlockSelected.value)
+		return ('fun-block-hidden');
+	else if (compBlockVisible.value)
+		return ('fun-block-visible');
+	return ('fun-block-selected');
+})
+
+const funSetClass = computed(() => {
+	if (funBlockSelected.value)
+		return ('fun-set-visible');
+	return ('fun-set-hidden');
+})
+
+const compSetClass = computed(() => {
+	if (compBlockSelected.value)
+		return ('comp-set-visible');
+	return ('comp-set-hidden');
 })
 </script>
 
 <style scoped>
-	/* .start_game {
-		@apply ;
-	} */
 
 	.slideshow {
-		@apply border mx-20 overflow-hidden flex items-center justify-center;
-
+		@apply w-screen flex items-center justify-center;
+		height: 100%;
+		/* top: 50%; */
+  		-ms-transform: translateY(25%);
+  		transform: translateY(25%);
 	}
 
 	.block-style {
@@ -42,7 +85,8 @@ const funBlockClass = computed(() => {
 	}
 
 	.block-style:hover {
-		@apply transform bg-white bg-opacity-10 scale-110;
+		/* @apply transform bg-white bg-opacity-10 scale-110; */
+		@apply bg-white bg-opacity-10;
 	}
 
 	.block-image {
@@ -57,8 +101,41 @@ const funBlockClass = computed(() => {
 		@apply opacity-100 translate-x-0;
 	}
 
+	.fun-block-selected {
+		@apply opacity-100 transform -translate-x-full;
+		@apply bg-white bg-opacity-10;
+	}
+
 	.fun-block-hidden {
+		@apply opacity-0 transform translate-x-full;
+	}
+
+	.fun-set-visible {
+		@apply opacity-100 transform -translate-x-full;
+	}
+
+	.fun-set-hidden {
+		@apply opacity-0 transform -translate-x-0;
+	}
+
+	.comp-block-visible {
+		@apply opacity-100 translate-x-0;
+	}
+	.comp-block-selected {
 		@apply opacity-100 transform translate-x-full;
+		@apply bg-white bg-opacity-10;
+	}
+
+	.comp-block-hidden {
+		@apply opacity-0 transform -translate-x-full;
+	}
+
+	.comp-set-visible {
+		@apply opacity-100 transform translate-x-full;
+	}
+
+	.comp-set-hidden {
+		@apply opacity-0 transform translate-x-0;
 	}
 
 	.block-style {
