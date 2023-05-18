abd2fd6d60895eeca64d7945a3a088fad0a450ac
rsiebert <rsiebert@student.42heilbronn.de>
2023-05-18 16:36:06 +0200

    chat restructed; help, global chat and kind of create work

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -7,7 +7,7 @@ import {
 	SubscribeMessage,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
-import { ChatService, User, Channel } from './chat.service';
+import { ChatService } from './chat.service';
 import { Users } from '../user/user.service';
 
 
@@ -23,39 +23,42 @@ export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
 	@WebSocketServer() server: Server;
 
 	afterInit(server: Server) {
-		this.chatService.addChannel("global", undefined, undefined);
+		this.chatService.addChannel("global", undefined, false, undefined);
 		console.log('Chat Initialized');
+	}
+
+	handleConnection(client: Socket, ...args: any[]) {
+		let intra = client.handshake.query.intra as string;
+		// FIXME this is for disallowing a conection in second tab
+		if (this.chatService.getIntraFromSocket(client) != intra) {
+			this.chatService.addUser(intra, client);
+			console.log(`Chat Client Connected: ${client.id}`);
+		}
+		else
+			client.disconnect(true);
 	}
 
 	handleDisconnect(client: Socket) {
 		console.log(`Chat Client Disconnected: ${client.id}`);
 	}
 
-	handleConnection(client: Socket, ...args: any[]) {
-		let intra = client.handshake.query.intra as string;
-		if (this.chatService.getIntraFromSocket(client) != intra) {
-			this.chatService.addUser(intra, client);
-			console.log(`Chat Client Connected: ${client.id}`);
-		}
-		else
-			client.disconnect();
-	}
-
-	/**
-	 * 	@response [0]: socket room to emit to
-	 * 	@response [1]: if command, usually the same prompt the user has entered with trailing newline. Otherwise empty.
-	 * 	@response [2]: response in form of message/command response/error message
-	 */
 	@SubscribeMessage("messageToServer")
 	handleMessageToServer(client: Socket, ...args: any[]) {
-		console.log("message args inside 'messageToServer' listener:", args[0]);
-		let user = this.chatService.getUser(client);
-		console.log("user:", user.getIntraname());
-		let username = user.getUsername() + ": ";
-		console.log("username:", user.getUsername());
-		let response = this.chatService.resolvePrompt(client, args[0]);
-		if (client.id == response[0])
-			username = "";
-		this.server.to(response[0]).emit("messageToClient", username, response[1] + response[2]);
+		let tokens: string[] = args[0].split(' ');
+		let response: [string, string, string];
+		if (tokens[0] == "/help" && tokens.length == 1)
+			response = this.chatService.help(client);
+		else if (tokens[0] == "/create" && (tokens.length == 2 || tokens.length == 3))
+			response = this.chatService.create(client, tokens[1], tokens[2], args[0]);
+		//else if (tokens[0] == "/join" && (tokens.length == 2 || tokens.length == 3))
+		//	response = this.chatService.join();
+		else if (tokens[0][0] == '/')
+			response = this.chatService.unknown(client, tokens[0]);
+		else
+			response = this.chatService.message(client, args[0]);
+		let recipient = response[0];
+		let sender = response[1]
+		let message_body = response[2];
+		this.server.to(recipient).emit("messageToClient", sender, message_body);
 	}
 }
--- backend/src/chat/chat.service.ts
+++ backend/src/chat/chat.service.ts
@@ -13,39 +13,10 @@ export class ChatService {
 	//		key: intraname
 	private members: Map<string, User> = new Map<string, User>;
 
-
-	public getIntraFromSocket(client: Socket): string {
-		for (let [intraname, user] of this.members) {
-			if (user.getSocket() == client)
-				return intraname;
-		}
-		return undefined;
-	}
-
-	public getChannelByChannelId(channel_id: string): Channel {
-		for (let [key, channel] of this.channels) {
-			if (key == channel_id)
-				return channel;
-		}
-		return undefined;
-	}
-
-	public getUser(client: Socket): User {
+	public addChannel(channel_id: string, client: Socket, open: boolean, password: string) {
 		let intra = this.getIntraFromSocket(client);
-		let user = this.members.get(intra);
-		return user;
-	}
-
-	public getUsername(client: Socket): string {
-		for (let [key, user] of this.members) {
-			if (user.getSocket() == client)
-				return user.getUsername();
-		}
-		return undefined;
-	}
-
-	public addChannel(channel_id: string, owner: User, password: string) {
-		this.channels.set(channel_id, new Channel(channel_id, owner, password));
+		let channel = new Channel(channel_id, intra, open, password);
+		this.channels.set(channel_id, channel);
 	}
 
 	public async addUser(intra: string, client: Socket) {
@@ -55,80 +26,89 @@ export class ChatService {
 		client.join("global");
 	}
 
-	public resolvePrompt(client: Socket, prompt: string): [string, string, string] {
-		let intra: string = this.getIntraFromSocket(client);
-		let user: User = this.members.get(intra);
-		let response: [string, string, string] = this.parsePrompt(prompt, user);
-		return response;
-	}
-
-	public channelExists(channel_id: string): boolean {
-		for (let [key, channel] of this.channels) {
-			if (key == channel_id)
-				return true;
-		}
-		return false;
-	}
-
-	private parsePrompt(prompt: string, user: User): [string, string, string] {
-		let tokens: string[] = prompt.split(' ', 3);
-		if (tokens[0] == "/help" && tokens.length == 1) {
-			console.log("Command 'HELP' was identified");
-			return this.help(prompt, user);
-		}
-		else if (tokens[0] == "/join") {
-			console.log("Command 'JOIN' was identified");
-			if (tokens[1] == undefined)
-				return [user.getSocket().id, prompt + "\n", "Error: no channel specified"]
-			return this.join(prompt, user, tokens[1], tokens[2]);
-		}
-		else if (tokens[0][0] == "/")
-			return [user.getSocket().id, prompt + "\n", "Error: Unknown command"];
-		else {
-			console.log("No command identified");
-			return [user.getActiveChannel(), "" , prompt];
-		}
-	}
-
-	private help(prompt: string, user: User): [string, string, string] {
-		console.log("'HELP' gets executed");
-		let response = "/join [channel] (password)\n";
-		response = response.concat("/lorem opt1\n");
-		response = response.concat("/ipsuim opt1 opt2 opt3\n");
-		return [user.getSocket().id, prompt + "\n", response];
-	}
-
-	// TODO working currently on join command. Here exist serveral issues that need to be debugged.
-	// ISSUE feels like the client gets stuck in infinite loop after joining a room a second time (or generally joining a created room)
-	private join(prompt: string, user: User, channel_id: string, channel_password: string): [string, string, string] {
-		console.log("'JOIN' gets executed");
-
-		let channel: Channel;
-
-		// JOIN CHANNEL
-		if (this.channelExists(channel_id) == true) {
-			channel = this.getChannelByChannelId(channel_id);
-			if (channel.isPrivate() == true) {
-				if (channel.checkPassword(channel_password) == false)
-					return [user.getSocket().id, prompt + "\n", "Error: Wrong password"]; // if channels exists and is password protected but user inputs no or wrong password
-			}
-			if (channel.isMember(user) == false)
-				channel.addMember(user);
-			user.setActiveChannel(channel_id); // TODO somehow fetch and show the channel history (if not global) to the chat box in fronted
-			return [channel_id, user.getUsername(), "entered the channel"]; // if channel exists and password check did not fail
-		}
-
-		// CREATE CHANNEL
-		this.addChannel(channel_id, user, channel_password);
-		let password_response = " as a private channel with password protection."
-		if (channel_password == undefined)
-			password_response = "";
-		channel = this.getChannelByChannelId(channel_id);
-		channel.addMember(user);
+	public joinChannel(client: Socket, channel_id: string) {
+		let user = this.getUserFromSocket(client);
+		client.leave(user.getActiveChannelId());
 		user.setActiveChannel(channel_id);
-		return [user.getSocket().id, "You created the channel " + channel_id, password_response]; // if channel did not exist prior and was created now, setting the user as owner
+		client.join(user.getActiveChannelId());
+	}
+
+	public getUserFromSocket (client: Socket): User {
+		for (let [intra, user] of this.members) {
+			if (user.getSocket() == client)
+				return user;
+		}
+		console.log("getUserFromSocket returned undefined");
+		return undefined;
+	}
+
+	public getIntraFromSocket (client: Socket): string {
+		for (let [intra, user] of this.members) {
+			if (user.getSocket() == client)
+				return intra;
+		}
+		console.log("getIntraFromSocket returned undefined");
+		return undefined;
+	}
+
+	public getChannelFromId(channel_id_to_find: string) {
+		for (let [channel_id, channel] of this.channels) {
+			if (channel_id_to_find == channel_id)
+				return channel;
+		}
+		console.log("getChannelFromId returned undefined");
+		return undefined;
+	}
+
+
+	/************************************** COMMANDS ***************************************/
+
+	message(client: Socket, message_body: string): [string, string, string] {
+		let user = this.getUserFromSocket(client);
+		let sender = user.getUsername() + ": ";
+		let recipient = user.getActiveChannelId();
+		return [recipient, sender, message_body];
+	}
+
+	unknown(client: Socket, command: string): [string, string, string] {
+		let user = this.getUserFromSocket(client);
+		let recipient = user.getSocket().id;
+		let sender = "Error: "
+		let message_body = "'" + command + "' is an unknown command or has wrong options.";
+		return [recipient, sender, message_body];
+	}
+
+	help(client: Socket): [string, string, string] {
+		let user = this.getUserFromSocket(client);
+		let recipient = user.getSocket().id;
+		let sender = "\nFloppy: \n"
+		let message_body ="[mandatory] (optional)\n";
+		message_body = message_body.concat("/help\n");
+		message_body = message_body.concat("/create [name] (passwd)\n");
+		message_body = message_body.concat("/join [channel] (passwd)\n");
+		message_body = message_body.concat("/dm [username] [message]\n");
+		return [recipient, sender, message_body];
+	}
+
+	create(client: Socket, channel_id: string, passwd: string, prompt: string): [string, string, string] {
+		let user = this.getUserFromSocket(client);
+		let recipient = user.getSocket().id;
+		let message_body: string;
+		let sender: string;
+		if (this.channels.get(channel_id) != undefined) {
+			sender = "Error: ";
+			message_body = "a channel with the name " + channel_id + " already exists.";
+			return [recipient, sender, message_body];
+		}
+		this.addChannel(channel_id, client, true, passwd);
+		this.joinChannel(client, channel_id);
+		sender = "Floppy: ";
+		message_body = "You created and joined channel " + channel_id + " as the owner!";
+		return [recipient, sender, message_body];
 	}
 }
+
+/************************************** USER ***************************************/
 
 export class User {
 	constructor(
@@ -144,86 +124,25 @@ export class User {
 		this.username = await this.users.getUsernameByIntra(this.intraname);
 	}
 
-	public getUsername(): string { if (this.username == undefined) this.updateUserData(); return this.username; }
-
-	public getIntraname(): string { return this.intraname; }
 	public getSocket(): Socket { return this.socket; }
-	public getActiveChannel(): string { return this.active_channel; }
+	public getActiveChannelId(): string { return this.active_channel; }
+	public getUsername(): string { return this.username; }
 
-	public setSocket(socket: Socket) { this.socket = socket; }
-	public setActiveChannel(channel: string) {
-		if (this.active_channel == "global" && channel != "global")
-			this.socket.leave("global");
-		else if (channel == "global" && this.active_channel != "global")
-			this.socket.join("global");
-		this.active_channel = channel;
-	}
+	public setActiveChannel(channel: string) { this.active_channel = channel; }
 }
 
-/**
- * our object for any chatroom (dm/global/channel)
- */
+
+/************************************** Channel ***************************************/
+
 export class Channel {
 	constructor(
 		private readonly channel_id: string,
-		private owner: User,
+		private owner: string,
+		private open: boolean,
 		private password: string
 	) {}
 
-	private members: Array<User> = new Array<User>;// TODO test if socket is changed in array when user changes
+	private members: Array<User> = new Array<User>;
 	private admins:	Array<User> = new Array<User>;
 	private chat_history:[user: User, message: string][];
-
-	public initChannel() {
-		this.admins.push(this.owner);
-	}
-
-	public getChannelId() { return this.channel_id; }
-
-	public isPrivate(): boolean {
-		if (this.password == undefined)
-			return false;
-		return true;
-	}
-
-	public isOwner(user: User): boolean {
-		if (user == this.owner)
-			return (true);
-		return (false);
-	}
-	
-	public addMember(user: User) {
-		this.members.push(user);
-		user.getSocket().join(this.channel_id);
-	}
-
-	public isAdmin(user: User): boolean {
-		for (let admin of this.admins)
-			if (user == admin) return (true);
-		return (false);
-	}
-
-	// IDK but maybe infinite loop here?
-	public isMember(user: User): boolean {
-		console.log(this.members);
-		this.members.forEach((member, index) => {
-			if (member == user)
-				return true;
-		});
-		return false;
-	}
-
-	public checkPassword(input: string): boolean {
-		if (input == this.password)
-			return (true);
-		return (false);
-	}
-
-	public getChatHistory(): [User, string][] { return (this.chat_history); }
-
-	public addMessage(user: User, message: string) {
-		if (this.chat_history.length > 42)
-			this.chat_history.shift();
-		this.chat_history.push([user, message]);
-	}
 }
